2b05232f09cb99c81c30c08188eb63c7b23b8a29
Spexso <ipekipek48@hotmail.com>
2023-06-29 16:58:13 +0300

    Texture Loader & env file addition

---
+++ src/images/images.js
@@ -0,0 +1,13 @@
+import dirtImg from './dir.jpg';
+import grassImg from './grass.jpg';
+import glassImg from './glass.png';
+import logImg from './log.jpg';
+import woodImg from './wood.png';
+
+export {
+    dirtImg,
+    grassImg,
+    glassImg,
+    woodImg,
+    logImg,
+}
